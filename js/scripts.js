c12063b8b5360f22c76ce0be27a6a725dc053d11
Sooraj <30319462+srj7@users.noreply.github.com>
2021-10-06 01:19:55 +0530

    Added a lot of cool stuff

--- js/scripts.js
+++ js/scripts.js
@@ -64,6 +64,7 @@
         $(this).removeClass("floating-label-form-group-with-focus");
       });
     });
+    
   
   })(jQuery); // End of use strict
   
---
+++ js/typing.js
@@ -0,0 +1,39 @@
+const typedSpan = document.getElementById("typed")
+const totype = ["Programmer", "Web Developer", "Cloud Professional", "Freelance College Project advisor", "Linux Lover","DevOps Enthusiasist"]
+
+const delayTyping_char = 100;
+const delayErasing_text = 150;
+const delayTyping_text = 1000;
+
+let totypeIndex = 0;
+let charIndex = 0;
+
+function typeText() {
+	if (charIndex < totype[totypeIndex].length) {
+		typedSpan.textContent += totype[totypeIndex].charAt(charIndex);
+		charIndex++;
+		setTimeout(typeText, delayTyping_char);
+	}
+	else {
+		setTimeout(eraseText, delayTyping_text);
+	}
+}
+
+function eraseText() {
+	if (charIndex > 0) {
+		typedSpan.textContent = totype[totypeIndex].substring(0, charIndex-1);
+		charIndex = charIndex-1;
+		setTimeout(eraseText, delayErasing_text);
+	}
+	else {
+		totypeIndex++;
+
+		if (totypeIndex >= totype.length)
+			totypeIndex = 0;
+			setTimeout(typeText, delayTyping_text);
+	}
+}
+
+window.onload = function() {
+	if (totype[totypeIndex].length) setTimeout(typeText, delayTyping_text);
+}
