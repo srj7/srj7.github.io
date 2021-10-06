d542959282db336fd713c9ca910ef7a4d76b5f71
Sooraj <30319462+srj7@users.noreply.github.com>
2021-10-06 22:12:46 +0530

    Not the final one ig

--- js/scripts.js
+++ js/scripts.js
@@ -35,20 +35,6 @@
       $('.navbar-collapse').collapse('hide');
     });
   
-    // Activate scrollspy to add active class to navbar items on scroll
-    $('body').scrollspy({
-      target: '#mainNav',
-      offset: 80
-    });
-  
-    // Collapse Navbar
-    var navbarCollapse = function() {
-      if ($("#mainNav").offset().top > 100) {
-        $("#mainNav").addClass("navbar-shrink");
-      } else {
-        $("#mainNav").removeClass("navbar-shrink");
-      }
-    };
     // Collapse now if page is not at top
     navbarCollapse();
     // Collapse the navbar when page is scrolled
